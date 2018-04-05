3caa327e064a8707d10205475ee332a1211f4eae
Amos L King <amos@binarynoggin.com>
2018-04-05 13:49:26 -0500

    Cleans up repeated config key and documentation
    Amos King @adkron <amos@binarynoggin.com>

--- lib/bamboo/config_adapter/email.ex
+++ lib/bamboo/config_adapter/email.ex
@@ -11,29 +11,33 @@ defmodule Bamboo.ConfigAdapter.Email do
         |> Mailer.deliver_now()
       end
   """
+  @config_key :config_adapter
 
   alias Bamboo.Email
 
   @spec get_config(Email.t) :: map
   def get_config(%Email{private: private}) do
-    Map.get(private, :config_adapter, %{})
+    Map.get(private, @config_key, %{})
   end
 
   @doc """
-    Used to add specific adapter configuration to an email
+  Replaces current config adapter dynamic configuration
 
-   # Example
+  # Example
 
-      iex> Bamboo.ConfigAdapter.Email.put_config(%Bamboo.Email{}, %{foo: :bar})
-      %Bamboo.Email{private: %{config_adapter: %{foo: :bar}}}
+    iex> Bamboo.ConfigAdapter.Email.put_config(%Bamboo.Email{}, %{foo: :bar})
+    %Bamboo.Email{private: %{config_adapter: %{foo: :bar}}}
 
-      iex> Bamboo.ConfigAdapter.Email.put_config(%Bamboo.Email{private: %{other: :baz}}, %{foo: :bar})
-      %Bamboo.Email{private: %{config_adapter: %{foo: :bar}, other: :baz}}
+    iex> Bamboo.ConfigAdapter.Email.put_config(%Bamboo.Email{private: %{other: :baz}}, %{foo: :bar})
+    %Bamboo.Email{private: %{config_adapter: %{foo: :bar}, other: :baz}}
+
+    iex> Bamboo.ConfigAdapter.Email.put_config(%Bamboo.Email{private: %{config_adapter: :bar}}, %{foo: :baz})
+    %Bamboo.Email{private: %{config_adapter: %{foo: :baz}}}
   """
   @spec put_config(Email.t, map) :: Email.t
   def put_config(%Email{} = email, config) do
-    Map.update(email, :private, %{config_adapter: config}, fn
-      current_private -> Map.put(current_private, :config_adapter, config)
+    Map.update(email, :private, %{@config_key => config}, fn
+      current_private -> Map.put(current_private, @config_key, config)
     end)
   end
 end
